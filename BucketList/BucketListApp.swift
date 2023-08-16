cb3a258d9132db3d268846dd322d8693739a3cb5
LuiArm <Luisarmendariz24@icloud.com>
2023-08-15 22:07:17 -0600

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by luis armendariz on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
