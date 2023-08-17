5354151388eaa155ac6586d93d995303295f1916
LuiArm <Luisarmendariz24@icloud.com>
2023-08-16 22:54:21 -0600

    Selecting and editing map locations

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -9,9 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 39.7932, longitude: -104.9903), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    @State private var locations = [Location]()
     
+    @State private var selectedPlace: Location?
     var body: some View {
-        Map(coordinateRegion: $mapRegion)
+        ZStack{
+            Map(coordinateRegion: $mapRegion,annotationItems: locations){location in
+                MapAnnotation(coordinate: location.coordinate){
+                    VStack{
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(Circle())
+                        
+                        Text(location.name)
+                            .fixedSize()
+                    }
+                    .onTapGesture {
+                        selectedPlace = location
+                    }
+                }
+            }
+                .ignoresSafeArea()
+            
+            Circle()
+                .fill(.blue)
+                .opacity(0.3)
+                .frame(width: 32,height: 32)
+            
+            VStack{
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    
+                    Button{
+                        let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+                        
+                        locations.append(newLocation)
+                    }label: {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+            }
+        }
+        .sheet(item: $selectedPlace){place in
+            EditView(location: place){newLocation in
+                if let index = locations.firstIndex(of: place){
+                    locations[index] = newLocation
+                }
+            }
+        }
     }
 }
 
